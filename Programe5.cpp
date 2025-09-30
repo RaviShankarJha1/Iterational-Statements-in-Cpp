5a20299a5895cb6a5ce0c5f6ed4d931dd6e1060f
RaviShankarJha1 <rjha81467@gmail.com>
2025-10-01 02:19:44 +0530

    Create Programe5.cpp

--- Programe5.cpp
+++ Programe5.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n = 5;
-    for (int i = n; i >= 1; i--)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
+int main() {
+    
+    int i,j,n,a;
+    int k=0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    
+    for(i=n;i>0;i--){
+        a=0;
+            while(a<=k){
+                cout<<" ";
+                a++;
+            }
+        
+        for(j=1;j<=i;j++){
+            cout<<"* ";
         }
-        cout << endl;
+        k++;
+        cout<<endl;
+        
     }
+    
+    for(i=2;i<=n;i++){
+        a=k-2;
+        while(a>=0){
+            cout<<" ";
+            a--;
+        }
+        for(j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        k--;
+        cout<<endl;
+        
+    }
+
     return 0;
 }
