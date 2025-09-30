dfe8a84ad2d53494bfcfdbf9ca2a4c49799d3965
RaviShankarJha1 <rjha81467@gmail.com>
2025-10-01 02:18:07 +0530

    Create Program2.cpp

--- Program2.cpp
+++ Program2.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "SIT" << endl;
+int main(){
+    
+    int i,j;
+    int n=5;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
     }
-    return 0;
 }
